effcd977688cae95730ce191631cd1cb2a28f3e2
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-01-27 16:26:33 +0200

    Add all missing fields

--- pocketapi/src/main/java/com/shem/pocketapi/data/Article.java
+++ pocketapi/src/main/java/com/shem/pocketapi/data/Article.java
@@ -1,26 +1,30 @@
 package com.shem.pocketapi.data;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
 /**
  * Created by smagnezi on 19/01/2017.
  */
 
 
 public class Article {
-//        public String item_id;
-//        public String resolved_id;
-//        public String given_url;
-//        public String resolved_url;
-//        public String given_title;
+    public String item_id;
+    public String resolved_id;
+    public String given_url;
+    public String resolved_url;
+    public String given_title;
     public String resolved_title;
-//        public String favorite;
-//        public String status;
-//        public String excerpt;
-//        public String is_article;
-//        public String has_image;
-//        public String has_video;
+    public String favorite;
+    public String status;
+    public String excerpt;
+    public String is_article;
+    public String has_image;
+    public String has_video;
     public String word_count;
-//        public String tags;
-//        public String authors;
-//        public String images;
-//        public String videos;
+    public JSONObject tags;
+    public JSONObject authors;
+    public JSONObject images;
+    public JSONObject videos;
 }
